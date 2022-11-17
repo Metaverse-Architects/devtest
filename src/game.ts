4c8f0e0dd68cb5ff4b5a048338d9191e9c8f39c7
RaphaelNdonga <nkibi53@gmail.com>
2022-11-17 22:43:23 +0300

    coin picking moves player to a different level

--- src/game.ts
+++ src/game.ts
@@ -1,11 +1,44 @@
 import { movePlayerTo } from "@decentraland/RestrictedActions"
 import { TriggerButton } from "./triggerButton"
+import * as utils from '@dcl/ecs-scene-utils'
+import { createCoin } from "./coin"
 
 const museum = new Entity()
 const museumcolliders = new Entity()
 const assets = new Entity()
 const rocketboard = new Entity()
 const portal = new TriggerButton()
+const portal2 = new TriggerButton()
+const portal3 = new TriggerButton()
+const coinShape = new GLTFShape('models/coin.glb')
+const coinPositions = [
+  new Vector3(10, 9, 9),
+  new Vector3(19, 9, 10),
+  new Vector3(8, 9, 8),
+  new Vector3(8, 9, 9)
+]
+const triggerBoxShape = new utils.TriggerBoxShape(
+  new Vector3(1.5, 3, 1.5),
+  new Vector3(0, 1, 0)
+)
+
+let counter = 0
+
+const addCounter = () => {
+  counter++
+  if (counter > 2) {
+    movePlayerTo(new Vector3(12, 24.88, 7.44), new Vector3(15.99, 9.68, 3.13))
+  }
+}
+
+for (const coinPosition of coinPositions) {
+  createCoin(
+    coinShape,
+    new Transform({ position: coinPosition }),
+    triggerBoxShape,
+    addCounter
+  )
+}
 
 
 museum.addComponent(new GLTFShape('models/YW_MainGeo_1.glb'))
@@ -13,26 +46,49 @@ museumcolliders.addComponent(new GLTFShape('models/YW_Colliders_1.glb'))
 assets.addComponent(new GLTFShape('models/YW_Assets_1.glb'))
 rocketboard.addComponent(new GLTFShape('models/rocketBoard.glb'))
 rocketboard.addComponentOrReplace(new Transform({
-  position: new Vector3(10.00,24.88,7.44),
+  position: new Vector3(10.00, 24.88, 7.44),
   scale: new Vector3(1, 1, 1),
   rotation: new Quaternion().setEuler(0.000, 0.000, 0.000),
 }))
 
 portal.addComponentOrReplace(new Transform({
-  position: new Vector3(16.07,1.19,7.39),
+  position: new Vector3(16.07, 1.19, 7.39),
   scale: new Vector3(1, 1, 1),
   rotation: new Quaternion().setEuler(0.000, 0.000, 0.000),
 }))
-portal.onClick = () =>{
+portal.onClick = () => {
   movePlayerTo(new Vector3(16.20, 9.68, 10.29), new Vector3(15.99, 9.68, 3.13))
 }
 //portal.removeComponent(BoxShape)
+portal2.addComponentOrReplace(new Transform({
+  position: new Vector3(10, 9.68, 10.29),
+  scale: new Vector3(1, 1, 1),
+  rotation: new Quaternion().setEuler(0.000, 0.000, 0.000),
+}))
+
+portal3.addComponentOrReplace(new Transform({
+  position: new Vector3(10, 18, 10.29),
+  scale: new Vector3(1, 1, 1),
+  rotation: new Quaternion().setEuler(0.000, 0.000, 0.000)
+}))
+
+
+
+portal2.onClick = () => {
+  movePlayerTo(new Vector3(12, 20, 7.39), new Vector3(15.99, 9.68, 3.13))
+}
+
+portal3.onClick = () => {
+  movePlayerTo(new Vector3(12, 24.88, 7.44), new Vector3(15.99, 9.68, 3.13))
+}
 
 engine.addEntity(portal)
 engine.addEntity(museum)
 engine.addEntity(museumcolliders)
 engine.addEntity(assets)
 engine.addEntity(rocketboard)
+engine.addEntity(portal2)
+engine.addEntity(portal3)
 
 
 
